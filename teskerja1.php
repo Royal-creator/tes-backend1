108d0ebb4e743661696c9aed5c81a393ce1e5fe2
Royal-creator <68028030+Royal-creator@users.noreply.github.com>
2020-07-09 02:00:42 +0700

    tes backend 1

---
+++ teskerja1.php
@@ -0,0 +1,193 @@
+<?php 
+
+	for($i=1; $i<=15; $i++){
+		if($i == 1 || $i == 3 || $i == 4 || $i == 5 || $i == 6 || $i == 7 || $i == 8 || $i == 9 || $i == 10 || $i == 11 || $i == 12 || $i == 13 || $i == 14 || $i == 15){
+			echo"@";
+		}else{
+			echo" &nbsp;&nbsp;&nbsp;";
+		}
+		
+
+	}
+
+	echo"<br>";
+
+	for($i=1; $i<=1; $i++){
+		if($i == 1){
+			echo"@";
+		}else{
+			echo"&nbsp;";
+		}
+		echo"&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;";
+	}
+
+	for($i=1; $i<=1; $i++){
+		if($i == 1 || $i == 15){
+			echo"@";
+		}else{
+			echo" &nbsp;&nbsp;";
+		}
+		echo" &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;";
+
+		echo"<br>";
+
+	}
+
+	for($i=1; $i<=15; $i++){
+		if($i == 1 || $i == 2 || $i == 3 || $i == 4 || $i == 5 || $i == 6 || $i == 7 || $i == 8 || $i == 9 || $i == 10 || $i == 11 || $i == 12 || $i == 13 ||  $i == 15){
+			echo"@";
+		}else{
+			echo" &nbsp;&nbsp;&nbsp;";
+		}
+		echo"";
+
+	}
+	echo"<br>";
+	for($i=1; $i<=2; $i++){
+		if($i == 1 || $i == 2){
+			echo"@";
+		}else{
+			echo"&nbsp;";
+		}
+		echo"&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;";
+	}
+
+	echo"<br>";
+
+	for($i=1; $i<=15; $i++){
+		if($i == 1 || $i == 3 || $i == 4 || $i == 5 || $i == 6 || $i == 7 || $i == 8 || $i == 9 || $i == 10 || $i == 11 || $i == 12 || $i == 13 || $i == 14 || $i == 15){
+			echo"@";
+		}else{
+			echo" &nbsp;&nbsp;&nbsp;";
+		}
+		
+
+	}
+
+	echo"<br>";
+
+	for($i=1; $i<=2; $i++){
+		if($i == 1 || $i == 2){
+			echo"@";
+		}else{
+			echo"&nbsp;";
+		}
+		echo"&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;";
+	}
+
+	echo"<br>";
+
+	for($i=1; $i<=15; $i++){
+		if($i == 1 || $i == 2 || $i == 3 || $i == 4 || $i == 5 || $i == 6 || $i == 7 || $i == 8 || $i == 9 || $i == 10 || $i == 11 || $i == 12 || $i == 13 ||  $i == 15){
+			echo"@";
+		}else{
+			echo" &nbsp;&nbsp;&nbsp;";
+		}
+		
+
+	}
+
+	echo"<br>";
+
+	for($i=1; $i<=2; $i++){
+		if($i == 1 || $i == 2){
+			echo"@";
+		}else{
+			echo"&nbsp;";
+		}
+		echo"&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;";
+	}
+
+	echo"<br>";
+
+	for($i=1; $i<=15; $i++){
+		if($i == 1 || $i == 3 || $i == 4 || $i == 5 || $i == 6 || $i == 7 || $i == 8 || $i == 9 || $i == 10 || $i == 11 || $i == 12 || $i == 13 || $i == 14 || $i == 15){
+			echo"@";
+		}else{
+			echo" &nbsp;&nbsp;&nbsp;";
+		}
+		
+
+	}
+
+	echo"<br>";
+
+	for($i=1; $i<=2; $i++){
+		if($i == 1 || $i == 2){
+			echo"@";
+		}else{
+			echo"&nbsp;";
+		}
+		echo"&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;";
+	}
+
+	echo"<br>";
+
+	for($i=1; $i<=15; $i++){
+		if($i == 1 || $i == 2 || $i == 3 || $i == 4 || $i == 5 || $i == 6 || $i == 7 || $i == 8 || $i == 9 || $i == 10 || $i == 11 || $i == 12 || $i == 13 ||  $i == 15){
+			echo"@";
+		}else{
+			echo" &nbsp;&nbsp;&nbsp;";
+		}
+		
+
+	}
+
+	echo"<br>";
+
+	for($i=1; $i<=2; $i++){
+		if($i == 1 || $i == 2){
+			echo"@";
+		}else{
+			echo"&nbsp;";
+		}
+		echo"&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;";
+	}
+
+	echo"<br>";
+
+	for($i=1; $i<=15; $i++){
+		if($i == 1 || $i == 3 || $i == 4 || $i == 5 || $i == 6 || $i == 7 || $i == 8 || $i == 9 || $i == 10 || $i == 11 || $i == 12 || $i == 13 || $i == 14 || $i == 15){
+			echo"@";
+		}else{
+			echo" &nbsp;&nbsp;&nbsp;";
+		}
+		
+
+	}
+
+	echo"<br>";
+
+	for($i=1; $i<=2; $i++){
+		if($i == 1 || $i == 2){
+			echo"@";
+		}else{
+			echo"&nbsp;";
+		}
+		echo"&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;";
+	}
+
+	echo"<br>";
+
+	for($i=1; $i<=14; $i++){
+		if($i == 1 || $i == 2 || $i == 3 || $i == 4 || $i == 5 || $i == 6 || $i == 7 || $i == 8 || $i == 9 || $i == 10 || $i == 11 || $i == 12 || $i == 13 ||  $i == 15){
+			echo"@";
+		}else{
+			echo" &nbsp;&nbsp;&nbsp;";
+		}
+		
+
+	}
+
+	for($i=1; $i<=1; $i++){
+		if($i == 1 || $i == 2 || $i == 3 || $i == 4 || $i == 5 || $i == 6 || $i == 7 || $i == 8 || $i == 9 || $i == 10 || $i == 11 || $i == 12 || $i == 13 || $i == 14 || $i == 15){
+			echo"@";
+		}else{
+		}
+		echo"<br>";
+	}
+	
+
+
+
+?>
